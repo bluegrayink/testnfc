ae6e77e52efe132da17734b870c56e1abc5b7b7c
bluegrayink <168587909+bluegrayink@users.noreply.github.com>
2024-08-27 03:22:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ if (!("NDEFReader" in window)) {
 // Lists of valid NFC UIDs for each page
 const uidToPageMap = {
     "miyuki.html": ["64019CB0", "F481A3B0"],
-    "rune.html": ["B405A0B0"],
+    "rune.html": ["B405A0B0", "54B0A4B0"],
     "gita.html": ["B4C3A1B0"]
 };
 
