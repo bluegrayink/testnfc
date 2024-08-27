5a19e99cdb02b6cd24a06320fcce22e84bab4b88
bluegrayink <168587909+bluegrayink@users.noreply.github.com>
2024-08-28 00:38:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ if (!("NDEFReader" in window)) {
 const uidToPageMap = {
     "miyuki.html": ["64019CB0", "175647BF", "F22F47BF"],
     "rune.html": ["B405A0B0", "CB9B4ABF", "1D044BBF"],
-    "gita.html": ["B4C3A1B0"]
+    "gita.html": ["B4C3A1B0", "C37947BF", "0BA547BF"]
 };
 
 // Event listener for scan button
