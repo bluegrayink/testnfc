4d210f284585430a2095c4adeaa7850c1fc277f6
bluegrayink <168587909+bluegrayink@users.noreply.github.com>
2024-12-19 03:04:27 +0700

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -3,8 +3,8 @@ const eventList = document.getElementById('event-list');
 const events = {
     '2024-12-21': ['21-22 Des : Comic Fiesta (CF) Malaysia - Booth Senyuki'],
     '2024-12-22': ['21-22 Des : Comic Fiesta (CF) Malaysia - Booth Senyuki'],
-    '2025-02-08': ['08-09 Feb : Mukashi - SPARK'],
-    '2025-02-09': ['08-09 Feb : Mukashi - SPARK']
+    '2025-2-8': ['08-09 Feb : Mukashi - SPARK'],
+    '2025-2-9': ['08-09 Feb : Mukashi - SPARK']
 }; // Object to store events
 
 const monthNames = [
