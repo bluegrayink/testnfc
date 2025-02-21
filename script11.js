c6911a8aab459dc8ed9cbaf171fdf459b18dbfff
bluegrayink <168587909+bluegrayink@users.noreply.github.com>
2025-02-22 01:06:41 +0700

    Update script11.js

--- script11.js
+++ script11.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const images = [
-        { id: "432F47BF", img_src: "raora.jpg" },
-        { id: "704248BF", img_src: "zeta.jpg" },
-        { id: "476F47BF", img_src: "nicole.jpg" }
+        { id: "432F47BF", img_src: "miyuki-raora01.jpg" },
+        { id: "704248BF", img_src: "miyuki-zeta01.jpg" },
+        { id: "476F47BF", img_src: "miyuki-nicole01.jpg" }
     ];
 
     const nfcData = localStorage.getItem("nfcData");
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (nfcData) {
         const image = images.find(img => img.id === nfcData);
         if (image) {
-            coverImage.style.backgroundImage = `url(Photo/${image.img_src})`;
+            coverImage.style.backgroundImage = `url(Photo/Miyuki/${image.img_src})`;
         } else {
             coverImage.innerHTML = "<p>Gambar tidak ditemukan</p>";
         }
